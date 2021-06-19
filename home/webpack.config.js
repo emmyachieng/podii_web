3524eae9bc1e8bc8f3f0f16ca1ab90b9f438457a
Japhetb <japhetbrandon@gmail.com>
2021-06-19 15:23:02 +0300

    partial html working

--- home/webpack.config.js
+++ home/webpack.config.js
@@ -1,35 +1,18 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path');
 
 module.exports = {
-  mode: "development",
-  entry: {
-    index: './src/index.js'
+mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
   },
-
-
-  devServer: {
-    port: 8080
-  },
-
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/html/index.html',
-      inject: true,
-      chunks: ['index'],
-      filename: 'index.html'
+        template: './src/index.html',
+        filename: 'index.html'
+
     })
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          "style-loader",
-          "css-loader"
-        ]
-      }
-    ]
-  },
-
+  ]
 };
-
